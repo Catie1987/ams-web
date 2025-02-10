6d9fd39ad94e9fa6e6130a81fb40153b5d0aba62
Catie1987 <hieu.nguyen912@gmail.com>
2025-02-10 16:50:38 +0700

    change

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -10,47 +10,47 @@ import arrowRightBold from '../../public/icons/arrow-right-bold.svg';
 import Image from 'next/image';
 
 export const Icons = {
-    vnflag: ({...props}) => {
+    vnflag: ({}) => {
         return(
             <Image alt='vn' src={vn} width={18} height={16}/>
         )
     },
-    enflag: ({...props}) => {
+    enflag: ({}) => {
         return(
             <Image alt='en' src={en} width={18} height={16}/>
         )
     },
-    globe: ({...props}) => {
+    globe: ({}) => {
         return(
             <Image alt='globe' src={globe} width={16} height={16}/>
         )
     },
-    errline: ({...props}) => {
+    errline: ({}) => {
         return(
             <Image alt='err' className='opacity-30' src={errline} width={800} height={600}/>
         )
     },
-    err: ({...props}) => {
+    err: ({}) => {
         return(
             <Image alt='err' src={err} width={434} height={194}/>
         )
     },
-    arrowRightWhite: ({...props}) => {
+    arrowRightWhite: ({}) => {
         return(
             <Image alt='arrow' src={arrowRightWhite} width={50} height={20}/>
         )
     },
-    arrowRightBlack: ({...props}) => {
+    arrowRightBlack: ({}) => {
         return(
             <Image alt='arrow' src={arrowRightBlack} width={50} height={20}/>
         )
     },
-    arrowLeftBlack: ({...props}) => {
+    arrowLeftBlack: ({}) => {
         return(
             <Image alt='arrow' src={arrowLeftBlack} width={50} height={20}/>
         )
     },
-    arrowRightBold: ({...props}) => {
+    arrowRightBold: ({}) => {
         return(
             <Image alt='arrow' src={arrowRightBold} width={20} height={20}/>
         )
